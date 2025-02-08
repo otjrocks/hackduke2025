c8f817b6381fe19d07c0324a5d2cf4ee468b6d8a
Owen Jennings <otj4@duke.edu>
2025-02-08 12:57:39 -0500

    added files

--- server/index.js
+++ server/index.js
@@ -2,8 +2,18 @@ const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 const dotenv = require('dotenv');
+const mongoose = require("mongoose");
 
 dotenv.config();
+
+// mongoose setup
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.MONGODB_API_URL;
+main().then(console.log("MongoDB connected")).catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 
 const app = express();
 
