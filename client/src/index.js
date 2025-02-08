41d580c1ad73793253a901c1d673b76c1ce99f03
Owen Jennings <otj4@duke.edu>
2025-02-08 15:41:09 -0500

    fix merge conflict

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import Home from "./Home"
 import Product from './components/Product';
 import AddProduct from "./components/AddProduct"
+import Profile from "./components/Profile"
 
 import {
   createBrowserRouter,
@@ -19,17 +20,14 @@ const router = createBrowserRouter([
   {
     path: "/product",
     element: <Product />
-<<<<<<< HEAD
   }, 
   {
     path: "/profile",
     element: <Profile />
-=======
   },
   {
     path: "/addproduct",
     element: <AddProduct />
->>>>>>> 81cd65e2afe939f412c0d77a69ee6e5976a00daa
   }
 ]);
 
