97dda9e77a59b16057b471331bb25ceedb02f84a
Julia Hornstein <juliahornstein@Julias-MacBook-Air-88.local>
2025-02-08 17:00:42 -0500

    fix merge conflict

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from "./Home"
 import Product from './components/Product';
-<<<<<<< HEAD
 import Browse from './components/Browse';
-=======
 import AddProduct from "./components/AddProduct"
 import Profile from "./components/Profile"
->>>>>>> 7883a407090bd9f0fe44da7b1db5d82d371e5818
 
 import {
   createBrowserRouter,
@@ -18,12 +15,10 @@ import './index.css';
 
 
 const router = createBrowserRouter([
-<<<<<<< HEAD
   { path: "/", element: <Home /> },
   { path: "/product", element: <Product /> },
   { path: "/browse", element: <Browse /> },
   // { path: "/theme/:theme", element: <ThemePage /> }
-=======
   {
     path: "/",
     element: <Home />,
@@ -40,7 +35,6 @@ const router = createBrowserRouter([
     path: "/addproduct",
     element: <AddProduct />
   }
->>>>>>> 7883a407090bd9f0fe44da7b1db5d82d371e5818
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
