3d2abb0abd3619ac8bba177d5e1bac4cd0176e08
Owen Jennings <otj4@duke.edu>
2025-02-08 13:14:52 -0500

    added router

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,5 +1,8 @@
+const express = require('express');
 const router = express.Router();  
 
 router.get("/", (req, res) => { 
     res.send('GET request to the product path');
 });
+
+module.exports = router;
