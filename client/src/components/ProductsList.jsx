a71dee9a366231d97682b09a629998b5c093a682
Owen Jennings <otj4@duke.edu>
2025-03-17 23:19:16 -0400

    style updates

--- client/src/components/ProductsList.jsx
+++ client/src/components/ProductsList.jsx
@@ -9,11 +9,11 @@ export default function ProductsList({ products }) {
                         key={product._id} 
                         className="product-item"
                     >
+                        {product.image && <img className="product-image" src={product.image} alt={product.name} />}
                         <h2>{product.name}</h2>
                         <p>Size: {product.size}</p>
                         <p>Price: ${product.price}</p>
                         <p>Email: <a href={`mailto:${product.email}`}>{product.email}</a></p>
-                        {product.image && <img src={product.image} alt={product.name} />}
                         <ShareButton url={process.env.REACT_APP_CLIENT_URL + "/product/" + product._id} title="Check out this amazing product on Campus Closet!"></ShareButton>
                     </div>
                 );
